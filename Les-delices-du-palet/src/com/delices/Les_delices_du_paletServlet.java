2b653e64de7fc0c66bdcf0e3186c9345c9b6c1b3
Mathieu Chailloux <fdrmc@free.fr>
2013-12-10 09:52:24 +0100

    test push

--- Les-delices-du-palet/src/com/delices/Les_delices_du_paletServlet.java
+++ Les-delices-du-palet/src/com/delices/Les_delices_du_paletServlet.java
@@ -8,6 +8,6 @@ public class Les_delices_du_paletServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		resp.getWriter().println("Hello, connard");
 	}
 }
