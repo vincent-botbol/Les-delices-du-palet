e5857bdaab57fe92b8aaedf07604eff5ca4427f5
vince <vince@vince-home>
2013-12-19 00:30:31 +0100

    Ajout des schémas jaxb (marshalling xml -> java)

---
+++ Les-delices-du-palet/src/test/MyTest.java
@@ -0,0 +1,33 @@
+package test;
+
+/*import java.io.File;
+
+ import javax.xml.bind.JAXBContext;
+ import javax.xml.bind.JAXBElement;
+ import javax.xml.bind.JAXBException;
+ import javax.xml.bind.Unmarshaller;
+
+ import org.xml.sax.SAXException;
+ */
+public class MyTest {
+
+	/*
+	 * public static void main(String[] args) throws JAXBException, SAXException
+	 * {
+	 * 
+	 * final JAXBContext jc = JAXBContext.newInstance("test.schedule");
+	 * 
+	 * Unmarshaller um = jc.createUnmarshaller();
+	 * 
+	 * @SuppressWarnings("unchecked") JAXBElement<LeagueType> st =
+	 * (JAXBElement<LeagueType>) um .unmarshal(new
+	 * File("data/nhl_v3_season_schedule_example.xml"));
+	 * 
+	 * LeagueType s = st.getValue();
+	 * 
+	 * System.out.println(s.getId());
+	 * System.out.println(s.getSeasonSchedule().getId());
+	 * 
+	 * }
+	 */
+}
